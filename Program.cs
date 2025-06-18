33bc209fe0d0cb0caea452af5e86504651205095
BlackCoder56 <blackcoda56@gmail.com>
2025-06-18 17:10:39 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿string first = "Hello";
+string second = "world"; ;
+// string result = string.Format("{0} {1}", first, second);
+Console.WriteLine("{1} {0}!", first, second);
+Console.WriteLine("{0} {0} {0}!", first, second);
